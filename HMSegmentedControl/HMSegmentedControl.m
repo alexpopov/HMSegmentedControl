2b4ae81a582949eb09111c4f68614986c8fc818a
Alex Popov <hello@alexpopov.ca>
2016-06-10 10:32:08 -0700

    Update HMSegmentedControl.m
    will trigger callback even if it's in the same segment

--- HMSegmentedControl/HMSegmentedControl.m
+++ HMSegmentedControl/HMSegmentedControl.m
@@ -715,7 +715,7 @@
             sectionsCount = [self.sectionTitles count];
         }
         
-        if (segment != self.selectedSegmentIndex && segment < sectionsCount) {
+        if (self.selectedSegmentIndex && segment < sectionsCount) {
             // Check if we have to do anything with the touch event
             if (self.isTouchEnabled)
                 [self setSelectedSegmentIndex:segment animated:self.shouldAnimateUserSelection notify:YES];
